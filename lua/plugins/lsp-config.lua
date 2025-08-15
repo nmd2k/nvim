bdcf48d6a03a90c105b31201bdb9b541a488048f
nmd2k <manhdung20112000@gmail.com>
2025-08-15 22:53:46 +0700

    add auto completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,41 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "rust_analyzer", "pyright"}
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+        lazy = false,
+		config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
             })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
+			lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set( { 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    },
+			vim.diagnostic.config({
+				virtual_text = true, -- inline messages
+				signs = true, -- symbols in the sign column
+				underline = true,
+				update_in_insert = false,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
