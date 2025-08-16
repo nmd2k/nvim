a6c4802b8fd82479ed0562672ffba179496b9d28
nmd2k <manhdung20112000@gmail.com>
2025-08-16 10:03:59 +0700

    add keymap

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -27,7 +27,9 @@ return {
 				},
 				mapping = cmp.mapping.preset.insert({
 					-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					-- ["<C-j>"] = cmp.mapping.scroll_docs(4),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
-return { 
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master',
-    lazy = false, 
-    build = ":TSUpdate", 
-    config = function()
-        local config = require('nvim-treesitter.configs') 
-        config.setup({
-          ensure_installed = {"lua", "python", "vim", "markdown", "vimdoc", "markdown_inline"},
-          highlight = { enable = true },
-          indent = { enable = true }
-        })
-    end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "python", "vim", "markdown", "vimdoc", "markdown_inline" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
