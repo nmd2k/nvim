e99755e7fdd7a209862f1c8de36023587954a2f7
nmd2k <manhdung20112000@gmail.com>
2025-08-18 23:48:26 +0700

    Limit preview size, ls parse for current file only

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,42 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
         lazy = false,
-		config = function()
+        config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-			lspconfig.pyright.setup({
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
 
-			vim.diagnostic.config({
-				virtual_text = true, -- inline messages
-				signs = true, -- symbols in the sign column
-				underline = true,
-				update_in_insert = false,
-			})
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            diagnosticMode = 'openFilesOnly',
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true,
+                        },
+                    },
+                },
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.diagnostic.config({
+                virtual_text = true, -- inline messages
+                signs = true, -- symbols in the sign column
+                underline = true,
+                update_in_insert = false,
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
-        "nvim-telescope/telescope.nvim", 
-        tag = '0.1.8', 
+        "nvim-telescope/telescope.nvim",
+        tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
@@ -10,13 +10,15 @@ return {
             -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
         end
     },
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
             require('telescope').setup({
+                defaults = {
+                    preview = { filesize_limit = 0.1 } -- in MB
+                },
                 extensions = {
                     ["ui-select"] = { require("telescope.themes").get_dropdown {} }
                 }
