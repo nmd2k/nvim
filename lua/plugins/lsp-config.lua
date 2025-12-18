46a82bde509dd729dee659cb5140b997b1b5591f
nmd2k <manhdung20112000@gmail.com>
2025-12-18 12:09:27 +1100

    update jedi

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "mason-org/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" },
+                ensure_installed = { "lua_ls", "pyright", "jedi-language-server" },
             })
         end,
     },
