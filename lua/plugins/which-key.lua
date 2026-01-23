78ed5cdd40430e5e0c8c3ffef6a9acf56bc6048e
nmd2k <manhdung20112000@gmail.com>
2026-01-23 16:55:55 +1100

    which-key update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,9 +1,9 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    opts = {},
     init = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 300 -- Show the popup after 300ms
-    end,
-    opts = {}
+    end
 }
