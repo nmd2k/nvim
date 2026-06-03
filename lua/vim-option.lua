55f5ff4ca456c8fe35abd62f3e706eacc5689759
nmd2k <manhdung20112000@gmail.com>
2026-06-03 11:28:17 +1000

    update keymap

--- lua/vim-option.lua
+++ lua/vim-option.lua
@@ -5,35 +5,12 @@ vim.cmd("set shiftwidth=4")
 
 vim.cmd("set number")
 vim.cmd("set rnu")
--- vim.cmd("set clipboard=unnamedplus")
 
--- -- Move lines up/down with Alt + j/k
--- vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
--- vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
---
--- -- Visual mode: move selected block
--- vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
--- vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
---
+-- Hide search result after hit Esc
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Neovim virtual text inside Vscode
--- vim.diagnostic.config({
---     virtual_text = true,  -- show inline
---     signs = true,
---     underline = true,
---     update_in_insert = false,
--- })
-
--- Delete not go into buffer
--- vim.keymap.set("n", "dd", '"_dd', { noremap = true })
--- vim.keymap.set("n", "dw", '"_dw', { noremap = true })
--- vim.keymap.set("n", "de", '"_de', { noremap = true })
--- vim.keymap.set("n", "dd", '"_dd')
--- vim.keymap.set("n", "de", '"_de')
--- vim.keymap.set("n", "d0", '"_d0')
--- vim.keymap.set("n", "d$", '"_d$')
--- vim.keymap.set("n", "dw", '"_dw')
--- vim.keymap.set("n", "cw", '"_cw')
+-- Pressing Enter in Normal mode creates a blank line below
+vim.keymap.set("n", "<CR>", "m`o<Esc>``", { silent = true })
 
 -- Quick escape from insert mode
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
