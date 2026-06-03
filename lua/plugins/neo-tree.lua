a3e4fd4a4841143f0a2f2e911942a18e281086e8
nmd2k <manhdung20112000@gmail.com>
2026-06-03 10:50:08 +1000

    Migrate

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,28 +1,22 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
 	keys = {
-		-- {
-		-- 	"<C-b>",
-		-- 	":Neotree filesystem position=left<CR>",
-		-- 	desc = "Open file explorer",
-		-- },
 		{
 			"<leader>e",
 			function()
 				-- If neo-tree is already loaded and we are in it, move to editor
 				if vim.bo.filetype == "neo-tree" then
-					vim.cmd.wincmd("l")
+					vim.cmd("Neotree close")
 				else
 					vim.cmd("Neotree focus")
 				end
 			end,
-			desc = "Toggle focus",
+			desc = "Toggle/Close focus",
 		},
 	},
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,7 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	lazy = false,
+	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
 			flavour = "frappe", -- latte, frappe, macchiato, mocha
