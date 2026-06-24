416a300c464df19d40b12a3c92e8ae7caa5d03f0
nmd2k <manhdung20112000@gmail.com>
2026-06-24 23:35:03 +1000

    Update lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -62,8 +62,21 @@ return {
 
 			local set = vim.keymap.set
 			set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+
+			if vim.g.vscode then
+				set("n", "gd", builtin.lsp_definitions, { desc = "Go to Definitions" })
+				set("n", "gr", builtin.lsp_references, { desc = "Go to References" })
+			else
+				-- Neovim setting
+				set("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "Go to Definitions" })
+				set("n", "grr", require("telescope.builtin").lsp_references, { desc = "Go to References" })
+			end
+
 			set("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "Go to Definitions" })
 			set("n", "grr", require("telescope.builtin").lsp_references, { desc = "Go to References" })
+
+			-- VScode setting
+			--
 		end,
 	},
 }
