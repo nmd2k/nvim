0f30d978941e1b447448a1fde880b6d7ee35ed02
nmd2k <manhdung20112000@gmail.com>
2026-07-16 10:20:54 +1000

    Solve keymaps conflict

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,10 +1,10 @@
 -- Require Floaterm installed
-vim.keymap.set("n", "<leader>ft", ":FloatermToggle<CR>", { silent = true, desc = "Toggle Floaterm" })
-vim.keymap.set("t", "<leader>ft", [[<C-\><C-n>:FloatermToggle<CR>]], { silent = true, desc = "Hide Floaterm" })
+vim.keymap.set("n", "<C-`>", ":FloatermToggle<CR>", { silent = true, desc = "Toggle Floaterm" })
+vim.keymap.set("t", "<C-`>", [[<C-\><C-n>:FloatermToggle<CR>]], { silent = true, desc = "Hide Floaterm" })
 
 local floaterm_zoomed = false
 
-vim.keymap.set({ "n", "t" }, "<Leader>tz", function()
+vim.keymap.set({ "n", "t" }, "<M-z>", function()
 	if floaterm_zoomed then
 		-- Zoom out (return to normal size)
 		vim.cmd("FloatermUpdate --width=0.7 --height=0.6")
