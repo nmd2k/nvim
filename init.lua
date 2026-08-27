cfc0ed11663ff6a4bf9791e2e615e75a78f76ff7
nmd2k <manhdung20112000@gmail.com>
2026-08-27 22:43:30 +1000

    Update vscode noti

--- init.lua
+++ init.lua
@@ -1,22 +1,28 @@
 if vim.loader then
 	vim.loader.enable()
 end
+
 -- Package Management
 require("utils.commands")
-require("pack")
 
 -- Editor Settings
 require("settings.options")
 require("settings.keymaps")
 
+if vim.g.vscode then
+    vim.o.cmdheight = 4
+end
+
 -- Plugins
 if not vim.g.vscode then
+    -- Load some default package
+    require("pack")
 	require("vim._core.ui2").enable({ enable = true })
 	require("plugins.telescope")
 	require("plugins.theme")
 	require("plugins.lsp")
 	require("plugins.cmp")
-	require("plugins.ui")
+	-- require("plugins.ui")
     require("plugins.terminal")
 
     -- Optional, require tree-sitter-cli
